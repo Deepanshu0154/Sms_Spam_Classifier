765125d136964100a3577d933a6aba56fa4420d0
Deepanshu Verma <95679855+Deepanshu0154@users.noreply.github.com>
2026-03-08 18:34:11 +0530

    Update ap.py

--- sms-spam-classifier/ap.py
+++ sms-spam-classifier/ap.py
@@ -37,8 +37,8 @@ BASE_DIR = os.path.dirname(__file__)
 vectorizer_path = os.path.join(BASE_DIR, "vectorizer.pkl")
 model_path = os.path.join(BASE_DIR, "model.pkl")
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open(vectorizer_path,'rb'))
+model = pickle.load(open(model_path,'rb'))
 
 
 st.title("Email/SMS Spam Classifier")
